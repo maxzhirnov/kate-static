f2a690c79c70e89396f43e47d44564545d3c9bd8
mzhirnov <maximz2009@gmail.com>
2023-08-03 15:22:52 +0300

    mob hover and mailto

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-function showInfo() {
-    var info = document.querySelector(".info");
-    if (info.style.display === "none") {
-      info.style.display = "block";
-    } else {
-      info.style.display = "none";
-    }
+function toggleVisibility(element) {
+  // Проверяем, используется ли мобильное устройство
+  var isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+
+  if (!isMobile) {
+    // Если не мобильное устройство, выходим из функции
+    return;
   }
-  
+
+  // Находим .hover-div внутри элемента, на который нажали
+  var hoverDiv = element.querySelector('.hover-info');
+
+  // Если .hover-div уже видим, то скрываем его
+  if (hoverDiv.classList.contains('visible')) {
+    hoverDiv.classList.remove('visible');
+  } else {
+    // Иначе делаем его видимым
+    hoverDiv.classList.add('visible');
+  }
+}
