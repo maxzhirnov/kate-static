4739303ba00593d9a065387b2def43b067b83709
mzhirnov <maximz2009@gmail.com>
2023-08-03 14:11:34 +0300

    initial

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showInfo() {
+    var info = document.querySelector(".info");
+    if (info.style.display === "none") {
+      info.style.display = "block";
+    } else {
+      info.style.display = "none";
+    }
+  }
+  
